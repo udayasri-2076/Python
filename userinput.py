ba5713d02d7af39da249e6b144a7f5331b22e04d
Udayasri <udayas1220@gmail.com>
2026-01-28 22:35:04 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,10 @@
+#input()= A function that prompts the user to enter data. 
+
+name=input("What is ur name: ")
+
+print(f"Hello {name}") #output:What is ur name: alex
+                               #Hello alex
+
+age=input("How old are you?")
+print(f"you are {age} years old")
+#output: 
