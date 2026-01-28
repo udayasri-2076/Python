d6355aa88fe9651ee1765493ffe94640147a7312
Udayasri <udayas1220@gmail.com>
2026-01-28 22:27:42 +0530

    Initial commit

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+#TypeCasting = the process of converting a variable from one data type to another str(),int(),float(),bool()
+
+name="ALEX"
+age=24
+gpa=9.8
+is_student=True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+#convert gpa to integer
+
+gpa=int(gpa)
+print(gpa)  #9 output
+
+
+age=float(age)
+print(age) #24.0 output
+
